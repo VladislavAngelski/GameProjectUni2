71b24e41ca32b686ec0d6b8244296478e53de2c9
VladislavAngelski <vladi_1243@abv.bg>
2021-06-08 23:10:52 +0300

    GameExtraRepository push all repositories are ready

---
+++ Repositories/GameExtraRepository/GameExtraRepository.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.GameExtraRepository
+{
+    public class GameExtraRepository : IGameExtraRepository
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        public GameExtraRepository(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void Add(GameExtra gameextra)
+        {
+            _appDbContext.Entry(gameextra).State = EntityState.Detached;
+            _appDbContext.GameExtras.Add(gameextra);
+            _appDbContext.SaveChanges();
+        }
+        public ICollection<GameExtra> GetGameExtras(int gameId) => _appDbContext.GameExtras.Include(x => x.Game)
+                                                                                      .Include(x => x.Extra)
+                                                                                      .Where(x => x.GameId == gameId).ToList();
+    }
+}
---
+++ Repositories/GameExtraRepository/IGameExtraRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.GameExtraRepository
+{
+    public interface IGameExtraRepository
+    {
+        void Add(GameExtra gameextra);
+        ICollection<GameExtra> GetGameExtras(int gameId);
+    }
+}
