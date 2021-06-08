cf11615c11f7f3a10b7e94ebc2b425492b814e5c
VladislavAngelski <vladi_1243@abv.bg>
2021-06-08 21:37:55 +0300

    GamePepository push

--- Repositories/GameRepository/GameRepository.cs
+++ Repositories/GameRepository/GameRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,25 @@ namespace Repositories.GameRepository
     {
         private readonly ApplicationDbContext _appDbContext;
         public GameRepository(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void Add(Game game)
+        {
+            _appDbContext.Games.Add(game);
+            _appDbContext.SaveChanges();
+
+        }
+        public void Update(Game game)
+        {
+            var oldGame = _appDbContext.Games.Single(x => x.Id == game.Id);
+            oldGame.Model = game.Model;
+            oldGame.Year = game.Year;
+            oldGame.SystemId = game.SystemId;
+            _appDbContext.Entry(oldGame).State = EntityState.Modified;
+            //_appDbContext.Games.Update(game);
+            _appDbContext.SaveChanges();
+
+        }
     }
 }
