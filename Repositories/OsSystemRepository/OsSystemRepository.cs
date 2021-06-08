cf7cc6c75962dbe1e399db01a3372ef172ffce3a
VladislavAngelski <vladi_1243@abv.bg>
2021-06-08 22:47:32 +0300

    Added OsSystem repositories

---
+++ Repositories/OsSystemRepository/OsSystemRepository.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.OsSystemRepository
+{
+    public class OsSystemRepository : IOsSystemRepository
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        public OsSystemRepository(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<OsSystem> GetAllOsSystems => _appDbContext.OsSystems.Include(x => x.Games)
+                                                                               .ToList();
+        public OsSystem GetOsSystemById(int id) => _appDbContext.OsSystems
+                                                                          .AsNoTracking()
+                                                                          .SingleOrDefault(x => x.Id == id);
+    }
+}
