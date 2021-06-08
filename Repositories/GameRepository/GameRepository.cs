53086d13397d3c071ffc3790a4ce5c604e23cd85
VladislavAngelski <vladi_1243@abv.bg>
2021-06-08 21:45:34 +0300

    Updated GameRepository

--- Repositories/GameRepository/GameRepository.cs
+++ Repositories/GameRepository/GameRepository.cs
@@ -16,6 +16,9 @@ namespace Repositories.GameRepository
         {
             _appDbContext = appDbContext;
         }
+        public IEnumerable<Game> GetAllGames => _appDbContext.Games.Include(x => x.OsSystem)
+                                                                   .Include(x => x.GamesExtras).ThenInclude(x => x.Extra)
+                                                                   .AsNoTracking().ToList();
         public void Add(Game game)
         {
             _appDbContext.Games.Add(game);
@@ -24,14 +27,19 @@ namespace Repositories.GameRepository
         }
         public void Update(Game game)
         {
-            var oldGame = _appDbContext.Games.Single(x => x.Id == game.Id);
+            Game oldGame = _appDbContext.Games.Single(x => x.Id == game.Id);
             oldGame.Model = game.Model;
             oldGame.Year = game.Year;
             oldGame.SystemId = game.SystemId;
             _appDbContext.Entry(oldGame).State = EntityState.Modified;
             //_appDbContext.Games.Update(game);
             _appDbContext.SaveChanges();
-
+        }
+        public void Delete(int id)
+        {
+            var game = _appDbContext.Games.Find(id);
+            _appDbContext.Games.Remove(game);
+            _appDbContext.SaveChanges();
         }
     }
 }
