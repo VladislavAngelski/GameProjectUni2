fb396750138027e10ce3e4404ce44bf51011e120
VladislavAngelski <vladi_1243@abv.bg>
2021-06-08 21:24:37 +0300

    GameRepository

--- Repositories/GameRepository/GameRepository.cs
+++ Repositories/GameRepository/GameRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace Repositories.GameRepository
 {
     public class GameRepository
     {
+        private readonly ApplicationDbContext _appDbContext;
+        public GameRepository(ApplicationDbContext appDbContext)
     }
 }
